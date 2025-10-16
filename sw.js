1cb2c255e4a0f2f2e98d06acc451a2500af544ec
evolutionofwellness-web <evolutionofwellness8@gmail.com>
2025-10-16 12:42:54 -0700

    Bump service worker cache version to v2
    Updated service worker to version 2 and added cache management.

--- sw.js
+++ sw.js
@@ -1,6 +1,16 @@
-const CACHE='reset-compass-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+const CACHE = 'reset-compass-v2'; // bumped to v2 to bust old cache
+const ASSETS = ['./','./index.html','./manifest.webmanifest'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE && caches.delete(k))))
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
 });
